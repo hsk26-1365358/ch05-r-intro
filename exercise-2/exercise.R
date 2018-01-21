7c27577eaf87e91a6b1c01b6d3aadc14d7f54e1a
HYEONG SUK KIM <hsk26@uw.edu>
2018-01-20 20:23:52 -0800

    EXERCISE-2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,31 +3,60 @@
 ## the game is called 'is it going to rain in Seattle?'
 
 ## make a variable called 'chance_of_rain' and assign it to be 0
+chance_of_rain <- 0
 
 ## assign 'true' or 'false' to a variable named 'I_saw_raindrops'
+I_saw_raindrops <- TRUE
 
 ## assign 'true' or 'false' to a variable named 'my_shoes_are_wet'
+my_shoes_are_wet <- FALSE
 
 ## assign 'true' or 'false' to a variable named 'my_ta_is_soaked'
+my_ta_is_soaked <- FALSE
 
 ## assign 'true' or 'false' to a variable named 'I_love_cookies'
+I_love_cookies <- TRUE
 
 ## ok, now you have your variables ready to go.
 
-## make an if statement that checks whether you saw raindrops
-## if you did, set chance_of_rain to 100
+if(I_saw_raindrops)
+{
+  chance_of_rain <- 100
+}else if(my_shoes_are_wet)
+{
+  chance_of_rain <- chance_of_rain + 40
+}else if(my_ta_is_soaked)
+{
+  chance_of_rain <- chance_of_rain + 50
+}
 
-## else make an if statement that checks whether your shoes are wet
-## if they are, chance of rain goes up by 40 percent
 
-## else make an if statement that checks whether your TA is soaked
-## if you did, chance of rain goes up by 50 percent
+if(!I_love_cookies){
+  print('I don\'t like cookies because I love icecreams more!')
+}
+cat('The chance of rain in Seattle is', chance_of_rain, 'percent')
 
-## make an if statement that checks whether you love cookies
-## if you don't, give a reason why not
 
-## Finally print out a sentence that says 'The chance of rain in Seattle is (the number stored in chance_of_rain variable) percent'
-## (You are doing predictive analysis here :-)
 
 ## bonus:
 ## how to make write this exercise in a function??
+ChanceOfRain <- function(I_saw_raindrops, my_shoes_are_wet, my_ta_is_soaked, I_love_cookies, chance_of_rain){
+  if(I_saw_raindrops){
+    chance_of_rain <- 100
+  
+  }else if(my_shoes_are_wet){
+    chance_of_rain <- chance_of_rain + 40
+  
+  }else if(my_ta_is_soaked){
+    chance_of_rain <- chance_of_rain + 50
+  }
+  
+  if(!I_love_cookies){
+    print('I don\'t like cookies because I love icecreams more!')
+  }
+  
+  cat('The chance of rain in Seattle is', chance_of_rain, 'percent')
+  
+}
+
+ChanceOfRain(FALSE, TRUE, TRUE, FALSE, 0)
